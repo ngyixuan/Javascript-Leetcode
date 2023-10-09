99c4ff34ad46ee7831a5344ddf3dfc417ec04625
ngyixuan <ngyixuan2009@gmail.com>
2023-10-09 23:10:03 +0800

    leetcode 120

--- kattis/IT5003ps1.py
+++ kattis/IT5003ps1.py
@@ -1,17 +1,33 @@
 
 
 
-from math import *
+# from math import *
 
-n= int(input())
-total = 0
-if n>11:
-     print("JUST RUN!!")
+# n= int(input())
+# total = 0
+# if n>11:
+#      print("JUST RUN!!")
+# else:
+#     for i in range(1,n+1):
+#         fact = factorial(n) /factorial(n-i)
+#         total+= fact
+#     print(int(total))
+
+
+totalPotions, timeConsume = input().split()
+timeEffect = 0
+for i in range(int(totalPotions)):
+    duration = int(input())
+    if(duration>timeEffect):
+        timeEffect=duration
+        
+if(int(totalPotions)*int(timeConsume) <timeEffect+int(timeConsume)):
+    print("YES")
 else:
-    for i in range(1,n+1):
-        fact = factorial(n) /factorial(n-i)
-        total+= fact
-    print(int(total))
+    print("NO")
+
+
+
 
         
 
